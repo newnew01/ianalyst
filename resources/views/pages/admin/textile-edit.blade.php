bb76e7e71d095fcd4ecc4d907d84dff688d90067
newnew01 <newnew01@gmail.com>
2017-11-07 00:11:30 +0700

    image upload fix

--- resources/views/pages/admin/textile-edit.blade.php
+++ resources/views/pages/admin/textile-edit.blade.php
@@ -46,7 +46,20 @@
     <script src="/js/tinymce/tinymce.min.js"></script>
     <script>
         tinymce.init({
-            selector:'#textile-detail' ,
+            selector: "#textile-detail",
+            plugins: [
+                "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+                "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
+                "table contextmenu directionality emoticons paste textcolor responsivefilemanager code"
+            ],
+            toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
+            toolbar2: "| responsivefilemanager | link unlink anchor | image media | forecolor backcolor  | print preview code ",
+            image_advtab: true ,
+
+            external_filemanager_path:"/filemanager/",
+            filemanager_title:"Responsive Filemanager" ,
+            external_plugins: { "filemanager" : "/filemanager/plugin.min.js"},
+            relative_urls: false
         });
     </script>
 @endsection
